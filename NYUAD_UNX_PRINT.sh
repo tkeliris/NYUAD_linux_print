ae402f7f477863a7fbe8b39fa27ac47464197456
Tasos Keliris <tasosk@gmail.com>
2017-10-18 11:34:38 +0400

    Packages updated for Ubuntu 16.04

--- NYUAD_UNX_PRINT.sh
+++ NYUAD_UNX_PRINT.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install cups printer-driver-hpcups printer-driver-postscript-hp python python-wxgtk3.0 python-wxtools wx3.0-i18n libwxgtk3.0-dev libgtk2.0-dev
+sudo apt-get install cups printer-driver-hpcups printer-driver-postscript-hp python python-wxgtk3.0 python-wxtools wx3.0-i18n libgtk3.0-cil-dev libwxgtk3.0-dev
 rm -rf ~/pharos && mkdir ~/pharos && pushd ~/pharos
 wget -c https://github.com/junaidali/pharos-linux/archive/master.zip && unzip master.zip && rm master.zip && cd pharos-linux-master
 wget http://pastebin.com/raw.php?i=UucbVj1V -O- | tr -d '\r' > printers.conf
